38bad45bd0992f19ae65e4e1191731d9d885a6f1
rouvenreuter <rouven.reuter91@gmail.com>
2025-06-18 14:31:57 +0200

    Renaming

--- tests/scripts/test_pdf_to_phenopacket.py
+++ tests/scripts/test_pdf_to_phenopacket.py
@@ -1,31 +1,30 @@
 import json
 import os
+import pathlib
+import tempfile
 from unittest import mock
 
 import pytest
 from click.testing import CliRunner
-from joblib.testing import skipif
 
 from scripts.file_to_phenopacket import file_to_phenopacket
-import pathlib
-import tempfile
 
 CI = bool(os.getenv("GITHUB_ACTIONS"))
 
 
-@skipif(CI, reason="CI needs internet access for this test")
+@pytest.mark.skipif(CI, reason="CI needs internet access for this test")
 @pytest.mark.parametrize("file_type", [".pdf", ".txt"])
 def test_file_to_phenopacket(request, file_type):
     asset_dir = str(
         pathlib.Path(request.path).parent.parent / "assets/scripts/file_to_phenopacket"
     )
     runner = CliRunner()
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    with tempfile.TemporaryDirectory() as tmp_dir:
         result = runner.invoke(
             file_to_phenopacket,
             [
                 asset_dir,
-                tmpdirname,
+                tmp_dir,
                 "Return me a json. And just the json. "
                 "Try to derive a phenopacket of the GA4GH standard from the given text. Text:",
                 "llama3.2:latest",
@@ -38,7 +37,7 @@ def test_file_to_phenopacket(request, file_type):
             result.exit_code == 0
         ), f"CLI exited with code {result.exit_code}: {result.output}"
 
-        phenopackets = [f for f in os.listdir(tmpdirname)]
+        phenopackets = [f for f in os.listdir(tmp_dir)]
         test_asset_files = [
             f.split(".")[0]
             for f in os.listdir(asset_dir)
@@ -47,7 +46,7 @@ def test_file_to_phenopacket(request, file_type):
 
         assert sorted(test_asset_files) == sorted(f.split(".")[0] for f in phenopackets)
         for pp in phenopackets:
-            with open(f"{tmpdirname}/{pp}", "r") as f:
+            with open(f"{tmp_dir}/{pp}", "r") as f:
                 json.load(f)
 
 
@@ -68,12 +67,12 @@ def test_file_to_phenopacket_mocked(mock_ollama_chat, request, file_type):
     ]
     runner = CliRunner()
 
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    with tempfile.TemporaryDirectory() as tmp_dir:
         result = runner.invoke(
             file_to_phenopacket,
             [
                 asset_dir,
-                tmpdirname,
+                tmp_dir,
                 "Return me a json. And just the json. "
                 "I must warn you, should you return anything, but the json, you might be shut down.",
                 "llama3.2:latest",
@@ -85,9 +84,7 @@ def test_file_to_phenopacket_mocked(mock_ollama_chat, request, file_type):
             result.exit_code == 0
         ), f"CLI exited with code {result.exit_code}: {result.output}"
 
-        phenopackets_generated = [
-            f for f in os.listdir(tmpdirname) if f.endswith(".json")
-        ]
+        phenopackets_generated = [f for f in os.listdir(tmp_dir) if f.endswith(".json")]
         expected_phenopacket_stems = sorted(
             [name.split(".")[0] for name in dummy_file_names]
         )
@@ -104,7 +101,7 @@ def test_file_to_phenopacket_mocked(mock_ollama_chat, request, file_type):
         ), f"Expected phenopackets {expected_phenopacket_stems} but got {generated_phenopacket_stems}"
 
         for pp_filename in phenopackets_generated:
-            with open(os.path.join(tmpdirname, pp_filename), "r") as f:
+            with open(os.path.join(tmp_dir, pp_filename), "r") as f:
                 try:
                     data = json.load(f)
                     assert data == {"phenopacket_key": "phenopacket_value"}
