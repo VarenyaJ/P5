ceab4805150eefb7f8bd4c50757171cbb969e51a
psnairne <patricknairne@hotmail.co.uk>
2025-06-23 15:03:39 +0200

    Altered fixtures location

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,40 +3,6 @@ import pytest
 
 CI = bool(os.getenv("GITHUB_ACTIONS"))
 
-
-@pytest.fixture()
-def pdf_bytes() -> bytes:
-    return (
-        b"%PDF-1.2\n"
-        b"1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n"
-        b"2 0 obj\n<< /Type /Pages /Count 1 /Kids [3 0 R] >>\nendobj\n"
-        b"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 250 50] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n"
-        b"4 0 obj\n<< /Length 51 >>\nstream\n"
-        b"BT /F1 20 Tf 72 20 Td (TEST) Tj ET\n"
-        b"endstream\nendobj\n"
-        b"5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n"
-        b"xref\n"
-        b"0 6\n"
-        b"0000000000 65535 f \n"
-        b"0000000010 00000 n \n"
-        b"0000000060 00000 n \n"
-        b"0000000113 00000 n \n"
-        b"0000000230 00000 n \n"
-        b"0000000317 00000 n \n"
-        b"trailer\n<< /Root 1 0 R /Size 6 >>\nstartxref\n401\n%%EOF"
-    )
-
-
 @pytest.fixture()
 def test_pmids() -> set:
     return {"PMID_8755636", "PMID_16636245"}
-
-
-@pytest.fixture()
-def test_pmids_no_pdf() -> set:
-    return {"PMID_16636245", "PMID_19458539"}
-
-
-@pytest.fixture()
-def test_pmids_with_pdf() -> set:
-    return {"PMID_8755636", "PMID_20089953"}
--- tests/scripts/test_PMID_downloader.py
+++ tests/scripts/test_PMID_downloader.py
@@ -13,6 +13,35 @@ import pickle
 
 CI = bool(os.getenv("GITHUB_ACTIONS"))
 
+@pytest.fixture()
+def pdf_bytes() -> bytes:
+    return (
+        b"%PDF-1.2\n"
+        b"1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n"
+        b"2 0 obj\n<< /Type /Pages /Count 1 /Kids [3 0 R] >>\nendobj\n"
+        b"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 250 50] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n"
+        b"4 0 obj\n<< /Length 51 >>\nstream\n"
+        b"BT /F1 20 Tf 72 20 Td (TEST) Tj ET\n"
+        b"endstream\nendobj\n"
+        b"5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n"
+        b"xref\n"
+        b"0 6\n"
+        b"0000000000 65535 f \n"
+        b"0000000010 00000 n \n"
+        b"0000000060 00000 n \n"
+        b"0000000113 00000 n \n"
+        b"0000000230 00000 n \n"
+        b"0000000317 00000 n \n"
+        b"trailer\n<< /Root 1 0 R /Size 6 >>\nstartxref\n401\n%%EOF"
+    )
+
+@pytest.fixture()
+def test_pmids_with_pdf() -> set:
+    return {"PMID_8755636", "PMID_20089953"}
+
+@pytest.fixture()
+def test_pmids_no_pdf() -> set:
+    return {"PMID_16636245", "PMID_19458539"}
 
 @pytest.mark.skipif(CI, reason="CI needs internet access for this test")
 def test_pmid_downloader(test_pmids, request):
