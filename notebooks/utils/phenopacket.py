d44c577c422da5b112623a2c261967e3687877d5
VarenyaJ <varenyajj@gmail.com>
2025-06-27 12:09:50 +0200

    Implement the `logging`

--- notebooks/utils/phenopacket.py
+++ notebooks/utils/phenopacket.py
@@ -1,11 +1,14 @@
 import json
 import copy
 import logging
+from logging import NullHandler
 from typing import Any, List
 from google.protobuf.json_format import ParseDict, ParseError
 from phenopackets import Phenopacket as ProtoPhenopacket
 
+# Create a module-named logger and attach a NullHandler so this library never prints logs unless an application specifically configures logging
 logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
 
 
 class InvalidPhenopacketError(ValueError):
@@ -36,16 +39,20 @@ class Phenopacket:
         InvalidPhenopacketError
             If validation or extraction fails for any reason.
         """
+        logger.debug("Initializing and Validating Phenopacket")
         try:
             _ = ParseDict(phenopacket_json, ProtoPhenopacket())
             feats = phenopacket_json["phenotypicFeatures"]
         except (TypeError, ParseError) as e:
+            logger.error("Failed to validate phenopacket: %s", e, exc_info=True)
             raise InvalidPhenopacketError(f"Failed to validate phenopacket: {e}")
 
         # If we reach this step then phenopacket_json is guaranteed to be valid, so we can successfully cache the raw JSON and the features list
         self._json = phenopacket_json
         # We assume the Protobuf-validated dict always has this key:
         self._phenotypicFeatures: List[dict[str, Any]] = feats
+        logger.info(f"Successfully validated %d phenotypic feature(s)", len(feats))
+        logger.debug("Successfully validated phenotypic feature(s): %r", feats)
 
     @classmethod
     def load_from_file(cls, path: str) -> "Phenopacket":
@@ -76,8 +83,10 @@ class Phenopacket:
         InvalidPhenopacketError
             If the JSON payload fails GA4GH schema validation.
         """
+        logger.debug("Loading Phenopacket from file: %s", path)
         with open(path, "r", encoding="utf-8") as f:
             data = json.load(f)
+        logger.info("Loaded JSON data from %s", path)
         return cls(data)
 
     def contains_phenotype(self, hpo_label: str) -> bool:
@@ -95,7 +104,9 @@ class Phenopacket:
             True if any phenotypic feature's `type.label` matches exactly,
             False otherwise.
         """
-        return hpo_label in self.list_phenotypes()
+        present = hpo_label in self.list_phenotypes()
+        logger.debug("Checking for phenotype %r: %s", hpo_label, present)
+        return present
 
     @property
     def count_phenotypes(self) -> int:
@@ -113,12 +124,15 @@ class Phenopacket:
         List[str]
             A list of phenotype labels, in insertion order.
         """
-        return [feat["type"]["label"] for feat in self._phenotypicFeatures]
+        labels = [feat["type"]["label"] for feat in self._phenotypicFeatures]
+        logger.debug("Listing phenotypes: %r", labels)
+        return labels
 
     def to_json(self) -> dict[str, Any]:
         """
         Return a deep copy of the original JSON dict to avoid external mutations because returning the exact same dict/internal state allows for accidental mutations
         """
+        logger.debug("Creating deep copy of original JSON")
         return copy.deepcopy(self._json)
 
     def __repr__(self) -> str:
